0be54031cfce85cc0145e02e8615eaf1e9f6ce96
Eshita Bhattacharya <eshita.bhattacharya@gmail.com>
2017-01-03 19:41:35 +0100

    first js file to git

---
+++ test.js
@@ -0,0 +1,9 @@
+var myFirstVariable = "Hello";
+
+// now I shall be printing the variable
+
+console.log(myFirstVariable);
+
+//How to make this run
+// Press Ctrl + Shift + B
+//then you will the result in shell below
